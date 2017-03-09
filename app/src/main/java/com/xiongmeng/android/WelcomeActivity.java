b84c873fd88ff5adf37d46e7c79af1749d27b1f7
xm890419 <506083998@qq.com>
2017-03-09 10:13:15 +0800

    欢迎页面

--- app/src/main/java/com/xiongmeng/android/WelcomeActivity.java
+++ app/src/main/java/com/xiongmeng/android/WelcomeActivity.java
@@ -1,14 +1,47 @@
 package com.xiongmeng.android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+import com.xiongmeng.android.activity.MainActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        },2000);
+    }
+
+    private void startMainActivity() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            startMainActivity();
+            return true;
+        }
+        return super.onTouchEvent(event);
     }
 }
---
+++ app/src/main/java/com/xiongmeng/android/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.xiongmeng.android.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.xiongmeng.android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
