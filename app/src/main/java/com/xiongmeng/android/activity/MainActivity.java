e356b7fb43d31f1373705e639672e56462050cdc
xm890419 <506083998@qq.com>
2017-03-09 11:26:18 +0800

    框架搭建

--- app/src/main/java/com/xiongmeng/android/activity/MainActivity.java
+++ app/src/main/java/com/xiongmeng/android/activity/MainActivity.java
@@ -1,15 +1,86 @@
 package com.xiongmeng.android.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
 
 import com.xiongmeng.android.R;
+import com.xiongmeng.android.frangment.BaseFrgment;
+import com.xiongmeng.android.frangment.HomeFragment;
+import com.xiongmeng.android.frangment.TypeFragment;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fl_main)
+    FrameLayout flMain;
+    @BindView(R.id.rg_main)
+    RadioGroup rgMain;
+    private ArrayList<BaseFrgment> frgments;
+
+    private int position;
+    private Fragment tempFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initFragment();
+        initListener();
+    }
+
+    private void initListener() {
+        rgMain.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rb_home:
+                        position = 0;
+                        break;
+                    case R.id.rb_type:
+                        position = 1;
+                        break;
+                }
+                Fragment currentFragment = frgments.get(position);
+                switchFragment(currentFragment);
+            }
+        });
+        rgMain.check(R.id.rb_home);
+    }
+
+    private void switchFragment(Fragment currentFragment) {
+        if (tempFragment != currentFragment) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            if (!currentFragment.isAdded()) {
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.add(R.id.fl_main, currentFragment);
+            } else {
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                ft.show(currentFragment);
+            }
+            ft.commit();
+
+            tempFragment = currentFragment;
+
+        }
+
+    }
+
+    private void initFragment() {
+        frgments = new ArrayList<>();
+        frgments.add(new HomeFragment());
+        frgments.add(new TypeFragment());
     }
 }
